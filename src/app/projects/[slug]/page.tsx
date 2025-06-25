8606aaf3223ea9f04974c6c676d942d3b69aa571
Kaori Kawakami <k.kawakami08@gmail.com>
2025-06-25 16:32:26 -0700

    updated unused imports

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -144,7 +144,7 @@ const ProjectPage = async ({ params }: ProjectPageProps) => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-2 bg-main-brand p-2 rounded-xl">
-        {project.images.map((image, index) => (
+        {project.images.map((image) => (
           <Image
             key={image.id}
             src={image.url}
